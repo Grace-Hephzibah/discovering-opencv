5b9c87572b97460bf9343dc9eb28a5a86a8d14c6
Grace-Hephzibah <mgracehephzibah@gmail.com>
2021-05-14 17:01:49 +0530

    Displaying the video

--- Chapter_01.py
+++ Chapter_01.py
@@ -2,13 +2,29 @@ import cv2
 
 # print("Package Imported")
 
+## VIDEO
+
+# Storing the video in a variable
+cap = cv2.VideoCapture("resources/city.mp4")
+
+# Displaying it in the output window as a series of images
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video Console", img)
+
+    # Making the video console screen to end when the letter 'q' is pressed
+    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        break
+
+## IMAGES
+
 # Storing the image in a variable
-img = cv2.imread("resources/girl.jpg")
+# img = cv2.imread("resources/girl.jpg")
 
 # Displaying the variable with the image in a window called "Output Window"
-cv2.imshow("Output Window", img)
+# cv2.imshow("Output Window", img)
 
 # The output window does not last long for view. So,
-cv2.waitKey(0)
+# cv2.waitKey(0)
 # 0 -> Infinite until window is closed
 # any other positive value is milli second ( Ex. 1000 = 1s )
